08f852e0feac7d49b2c90139fb017af72f63010d
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2019-10-14 16:44:54 +0800

    updated comment for the bmi class

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,8 +2,9 @@ import pandas
 
 class BodyMassIndexCalculator:
     """
-    Body Mass Calculator is a personal project that I just developed for fun and hoping that it can help you
-    with computing your Body Mass Index in order to know if you are overweight, . 
+    Body Mass Calculator is a personal project that I just developed for fun and hoping that it can help with 
+    computing your Body Mass Index in order to know if you are very severely underweight, severely underweight, 
+    overweight, underweight, normal (healthy weight), moderately obese, severely obese or very severely obese.
     """
     
     # Constructor
